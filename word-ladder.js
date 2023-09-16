c29cbf08d7978bc2e9cdb37fe0a2c1c2198daefd
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-16 22:34:59 +0530

    Create word-ladder.js

---
+++ word-ladder.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function (beginWord, endWord, wordList) {
+    const wordSet = new Set(wordList);
+    if (!wordSet.has(endWord)) return 0;
+
+    const queue = [];
+    queue.push(beginWord);
+
+    let level = 1;
+
+        for (let i = 0; i < size; i++) {
+            const currentWord = queue.shift();
+
+            if (currentWord === endWord) return level;
+
+            for (let j = 0; j < currentWord.length; j++) {
+                for (let k = 97; k <= 122; k++) {
+                    const newWord = currentWord.slice(0, j) + String.fromCharCode(k) + currentWord.slice(j + 1);
+
+                    if (wordSet.has(newWord)) {
+                        queue.push(newWord);
+                        wordSet.delete(newWord);
+                    }
+                }
+            }
+        }
+
+        level++;
+    }
+
+    return 0;
+};
