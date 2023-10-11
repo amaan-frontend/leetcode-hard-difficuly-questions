5156c1972c4fc229345086fff6ff0cda04279716
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-11 23:13:26 +0530

    Update number-of-flowers-in-full-bloom.js

--- number-of-flowers-in-full-bloom.js
+++ number-of-flowers-in-full-bloom.js
@@ -1,1 +1,16 @@
 
+const sortedFlowers = flowers.sort((a, b) => a[1] - b[1]);
+    const sortedPeople = people.sort((a, b) => a - b);
+    const result = [];
+    let flowerIndex = 0;
+    let bloomCount = 0;
+
+    for (let i = 0; i < sortedPeople.length; i++) {
+        while (flowerIndex < sortedFlowers.length && sortedFlowers[flowerIndex][1] <= sortedPeople[i]) {
+            bloomCount++;
+            flowerIndex++;
+        }
+        result.push(bloomCount);
+    }
+
+    return result;
