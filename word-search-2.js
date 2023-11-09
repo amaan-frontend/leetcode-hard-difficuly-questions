c4454cf758cfd49ecf23cfba9eb6535153790f3d
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-09 23:17:49 +0530

    Update word-search-2.js

--- word-search-2.js
+++ word-search-2.js
@@ -1,1 +1,12 @@
+class TrieNode {
+    constructor() {
+        this.children = new Map();
+        this.isEndOfWord = false;
+    }
+}
 
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+…}
+                                                                                                                                                                                            
