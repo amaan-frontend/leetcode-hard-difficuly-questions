409d015dd14ed5969e159edb5e6ee7b0c8794a0c
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-09 23:18:52 +0530

    Update maximal rectangle.js

--- maximal rectangle.js
+++ maximal rectangle.js
@@ -1,1 +1,11 @@
+function maximalRectangle(matrix) {
+    if (matrix.length === 0) return 0;
 
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+    const heights = Array(numCols).fill(0);
+    let maxArea = 0;
+
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+…                                                                                                                                                                                    
