cb9670edc60cf5768b69f0f1f1bb18aaada93953
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-10 22:16:47 +0530

    Update orderly-queue.js

--- orderly-queue.js
+++ orderly-queue.js
@@ -1,1 +1,13 @@
 
+function getLexicographicallySmallestString(s, k) {
+    let result = '';
+
+    while (k > 0) {
+        let smallestChar = s[0];
+        let smallestIndex = 0;
+
+        for (let i = 1; i <= k; i++) {
+            if (s[i] < smallestChar) {
+                smallestChar = s[i];
+…    return result;
+}
