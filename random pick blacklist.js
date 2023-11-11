9dee6a7bbe33054347ededce431c4edadb87579d
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-11 23:56:16 +0530

    Update random pick blacklist.js

--- random pick blacklist.js
+++ random pick blacklist.js
@@ -1,1 +1,17 @@
+class Solution {
+    constructor(n, blacklist) {
+        this.n = n;
+        this.blacklist = new Set(blacklist);
+        this.range = n - this.blacklist.size;
+        this.mapping = {};
+
+        for (let num of blacklist) {
+            if (num < this.range) {
+                while (this.blacklist.has(this.range - 1)) {
+…        if (this.mapping[rand] !== undefined) {
+            return this.mapping[rand];
+        }
+        return rand;
+    }
+}
 
