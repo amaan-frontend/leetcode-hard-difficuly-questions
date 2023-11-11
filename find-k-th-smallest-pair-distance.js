adcd75f4e1db169b887399e591cdc16419c95f15
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-11 23:57:46 +0530

    Update find-k-th-smallest-pair-distance.js

--- find-k-th-smallest-pair-distance.js
+++ find-k-th-smallest-pair-distance.js
@@ -1,1 +1,14 @@
 
+function smallestDistancePair(nums, k) {
+    nums.sort((a, b) => a - b);
+
+    let left = 0;
+    let right = nums[nums.length - 1] - nums[0];
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        let count = countPairs(nums, mid);
+
+…
+    return count;
+}
