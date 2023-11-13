39dae29675230292eda00f8786312e94a4fa942c
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-13 22:51:30 +0530

    Update n-queens-2.js

--- n-queens-2.js
+++ n-queens-2.js
@@ -1,1 +1,17 @@
+var totalNQueens = function (n) {
+    let count = 0;
+    const board = Array.from({ length: n }, () => Array(n).fill('.'));
+
+    function isSafe(row, col) {
+        for (let i = 0; i < row; i++) {
+            if (board[i][col] === 'Q') {
+                return false;
+            }
+        }
+…        }
+    }
+
+    solve(0);
+    return count;
+};
 
