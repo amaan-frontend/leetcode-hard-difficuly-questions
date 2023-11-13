1be427f7cd1fc92ae89ecb9438a49406cc1c4ea7
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-13 22:50:32 +0530

    Update n-queens.js

--- n-queens.js
+++ n-queens.js
@@ -1,1 +1,19 @@
+var solveNQueens = function (n) {
+    const result = [];
+    const board = Array.from({ length: n }, () => Array(n).fill('.'));
+
+    function isSafe(row, col) {
+        for (let i = 0; i < row; i++) {
+            if (board[i][col] === 'Q') {
+                return false;
+            }
+        }
+…                board[row][col] = '.';
+            }
+        }
+    }
+
+    solve(0);
+    return result;
+};
 
