d0b03d74b4503a0c256606c87ce57bdf4705cafd
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-14 23:28:54 +0530

    Update maximum-frequency-stack.js

--- maximum-frequency-stack.js
+++ maximum-frequency-stack.js
@@ -1,1 +1,32 @@
 
+class FreqStack {
+    constructor() {
+        this.freqMap = new Map();
+        this.groupMap = new Map();
+        this.maxFreq = 0;
+    }
+
+    push(val) {
+        const freq = (this.freqMap.get(val) || 0) + 1;
+        this.freqMap.set(val, freq);
+
+        if (!this.groupMap.has(freq)) {
+            this.groupMap.set(freq, []);
+        }
+        this.groupMap.get(freq).push(val);
+
+        this.maxFreq = Math.max(this.maxFreq, freq);
+    }
+
+    pop() {
+        const mostFrequentGroup = this.groupMap.get(this.maxFreq);
+        const val = mostFrequentGroup.pop();
+        this.freqMap.set(val, this.freqMap.get(val) - 1);
+
+        if (mostFrequentGroup.length === 0) {
+            this.maxFreq--;
+        }
+
+        return val;
+    }
+}
