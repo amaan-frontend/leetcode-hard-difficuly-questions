012d8f4fd005777a4ba35c2889fa2df8ee204073
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-16 23:15:48 +0530

    Update recover-a-tree-from-preorder-traversal.js

--- recover-a-tree-from-preorder-traversal.js
+++ recover-a-tree-from-preorder-traversal.js
@@ -1,1 +1,44 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+var recoverFromPreorder = function (traversal) {
+    const stack = [];
+    let i = 0;
+
+    while (i < traversal.length) {
+        let depth = 0;
+        while (i < traversal.length && traversal[i] === '-') {
+            depth++;
+            i++;
+        }
+
+        let value = 0;
+        while (i < traversal.length && traversal[i] !== '-') {
+            value = value * 10 + parseInt(traversal[i]);
+            i++;
+        }
+
+        const node = new TreeNode(value);
+
+        while (stack.length > depth) {
+            stack.pop();
+        }
+
+        if (stack.length > 0) {
+            if (!stack[stack.length - 1].left) {
+                stack[stack.length - 1].left = node;
+            } else {
+                stack[stack.length - 1].right = node;
+            }
+        }
+
+        stack.push(node);
+    }
+
+    return stack[0];
+};
 
