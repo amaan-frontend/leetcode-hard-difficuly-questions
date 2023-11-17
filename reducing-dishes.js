85808bf0e695ec202593bc660c1df72b14f544af
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-17 23:34:03 +0530

    Update reducing-dishes.js

--- reducing-dishes.js
+++ reducing-dishes.js
@@ -1,1 +1,16 @@
+function maxSatisfaction(satisfaction) {
+    satisfaction.sort((a, b) => b - a);
+
+    let maxSum = 0;
+    let currentSum = 0;
+
+    for (const s of satisfaction) {
+        currentSum += s;
+        if (currentSum > 0) {
+            maxSum += currentSum;
+        }
+    }
+
+    return maxSum;
+}
 
