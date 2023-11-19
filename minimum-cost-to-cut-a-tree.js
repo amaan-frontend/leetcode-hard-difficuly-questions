03b215829354b9edb2fa5c4027247eb650d90351
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-19 23:40:40 +0530

    Update minimum-cost-to-cut-a-tree.js

--- minimum-cost-to-cut-a-tree.js
+++ minimum-cost-to-cut-a-tree.js
@@ -1,1 +1,22 @@
 
+var minCost = function (n, cuts) {
+    cuts.sort((a, b) => a - b); 
+    cuts.unshift(0);
+    cuts.push(n);
+    const memo = {}; 
+    return rec(0, cuts.length - 1);
+    function rec(start, end) {
+        if (start + 1 === end) return 0; 
+        const memoKey = `${start}-${end}`;
+        if (memo[memoKey] !== undefined) return memo[memoKey];
+        let minCost = Infinity; 
+        for (let i = start + 1; i < end; i++) { 
+            const cost = cuts[end] - cuts[start] + rec(start, i) + rec(i, end);
+            minCost = Math.min(minCost, cost);
+        }
+        memo[memoKey] = minCost; 
+        return minCost;
+    }
+};
+                                                                            
+                                                                            
