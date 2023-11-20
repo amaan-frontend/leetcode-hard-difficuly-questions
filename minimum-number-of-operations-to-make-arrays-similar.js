717408e61c957b66352e3f6f2a6a70e7a148a86a
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-20 23:29:47 +0530

    Update minimum-number-of-operations-to-make-arrays-similar.js

--- minimum-number-of-operations-to-make-arrays-similar.js
+++ minimum-number-of-operations-to-make-arrays-similar.js
@@ -1,1 +1,15 @@
+var makeSimilar = function (nums, target) {
+    const isEven = num => num % 2 === 0;
+    const isOdd = num => num % 2 === 1;
 
+    const sortedNums = nums.sort((a, b) => a - b);
+    const sortedTarget = target.sort((a, b) => a - b);
+
+    const numsEven = sortedNums.filter(isEven);
+    const targetEven = sortedTarget.filter(isEven);
+    const numsOdd = sortedNums.filter(isOdd);
+…    }
+
+    return numberOfOperations / 2;
+};
+                                        
