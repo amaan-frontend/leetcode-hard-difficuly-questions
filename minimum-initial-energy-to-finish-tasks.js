fc313d50df411b7b747fac5720eef63cc31c5f9a
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-12-17 22:41:17 +0530

    Update minimum-initial-energy-to-finish-tasks.js

--- minimum-initial-energy-to-finish-tasks.js
+++ minimum-initial-energy-to-finish-tasks.js
@@ -1,1 +1,21 @@
+const minimumEffort = (tasks) => {
+    tasks.sort((a, b) => (b[1] - b[0]) - (a[1] - a[0]));
 
+    let minimumEnergy = tasks[0][1];
+    let actualEnergy = minimumEnergy - tasks[0][0];
+
+    for (let i = 1; i < tasks.length; i++) {
+        const nextMinEnergy = tasks[i][1];
+        const nextActualEnergy = tasks[i][0];
+
+        if (actualEnergy < nextMinEnergy) {
+            minimumEnergy += nextMinEnergy - actualEnergy;
+            actualEnergy = nextMinEnergy - nextActualEnergy;
+        }
+        else {
+            actualEnergy -= nextActualEnergy;
+        }
+    }
+
+    return minimumEnergy;
+};
