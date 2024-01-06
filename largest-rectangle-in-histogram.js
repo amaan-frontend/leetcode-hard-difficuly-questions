185a437183d2d65be5f3a020cd3a6f48787b2740
frontender <134868734+amaan-frontend@users.noreply.github.com>
2024-01-06 23:40:27 +0530

    Update largest-rectangle-in-histogram.js

--- largest-rectangle-in-histogram.js
+++ largest-rectangle-in-histogram.js
@@ -1,1 +1,14 @@
+var largestRectangleArea = function (heights) {
+    let leftStack = [];
+    let stack = [];
 
+    for (let i = 0; i < heights.length; i++) {
+        while (stack.length !== 0 && heights[stack[stack.length - 1]] >= heights[i]) {
+            stack.pop();
+        }
+  for (let i = 0; i < heights.length; i++) {
+        area = (rightStack[heights.length - i - 1] - leftStack[i] - 1) * heights[i];
+        max = Math.max(area, max);
+    }
+    return max;
+};
